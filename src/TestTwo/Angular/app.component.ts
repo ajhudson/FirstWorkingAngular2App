6137b5b2b92717beaba843a46cc3662d54e3ac92
Andy Hudson <andy.hudson@kaonix.com>
2016-02-15 13:10:29 +0000

    First commit

---
+++ src/TestTwo/Angular/app.component.ts
@@ -0,0 +1,9 @@
+﻿import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h2>Hello world, this is Andy\'s first Angular 2 app in VS2015</h2>'
+})
+export class AppComponent {
+
+}
